524b195f30ea6d8347a6805feec8a81f7af728c0
Kussy Leo <kussynorbertleo@gmail.com>
2022-08-02 09:49:35 +0200

    feat(app): PricingHeader re-added  #1

--- src/views/PricingPage.vue
+++ src/views/PricingPage.vue
@@ -2,6 +2,7 @@
   <div class="bg-white">
     <div class="relative">
       <main>
+        <PricingHero />
         <PricingSectionOnce />
         <!-- PricingOnceDescription -->
         <PricingSectionMonthly />
@@ -11,6 +12,7 @@
 </template>
 
 <script setup lang="ts">
+import PricingHero from '@/components/pricing/PricingHero.vue'
 import PricingSectionMonthly from '@/components/pricing/PricingSectionMonthly.vue'
 import PricingSectionOnce from '@/components/pricing/PricingSectionOnce.vue'
 </script>
